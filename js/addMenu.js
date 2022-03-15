eb4bb31b8806efb3a2c5caf180daa56d7674ea6a
unknown <nguyenshin1711@gmail.com>
2022-03-15 19:56:13 +0700

    first commit

---
+++ js/addMenu.js
@@ -0,0 +1,3 @@
+document.querySelector('#mb-menu-toggle').addEventListener('click', () => document.querySelector('#header-wrapper').classList.add('active'))
+
+document.querySelector('#mb-menu-close').addEventListener('click', () => document.querySelector('#header-wrapper').classList.remove('active'))
